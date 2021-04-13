9b7a208bdaa44e0e6b9316ebb834e49832091b4e
milos381 <milos_popovic@outlook.com>
2021-04-13 00:27:03 -0400

    commit tree 649c542a839baa77cfd2b73d0a943c8eb37b9248

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,5 +5,30 @@ import MovieReviews from './MovieReviews'
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
-
-// Code LatestMovieReviewsContainer Here
+ 
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+ 
+    this.state = {
+        reviews: []
+    };
+  }
+ 
+  componentDidMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then(reviewsData => this.setState({ reviews: reviewsData.results }))
+  }
+ 
+  render() {
+    return (
+        <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+        </div>
+    )
+  }
+}
+ 
+export default LatestMovieReviewsContainer;
